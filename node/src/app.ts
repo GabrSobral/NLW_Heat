e96407de7d66ab196eae578686246713a3545930
Sobraloser <Asgoth55@gmail.com>
2021-10-19 07:42:27 -0300

    initial commit

---
+++ node/src/app.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express, { response } from 'express'
+
+const app = express()
+
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get('/signin/callback', (request, response) => {
+  const { code } = request.query
+  return response.json(code)
+})
+
+const PORT  = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
